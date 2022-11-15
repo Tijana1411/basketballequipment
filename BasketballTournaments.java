cc8a887aecdb93a9821d205577a3764177c9e8e1
zvjezdanajelovac <ratkovac.zvjezdana@gmail.com>
2022-11-15 12:31:41 +0100

    adding project

---
+++ BasketballTournaments.java
@@ -0,0 +1,13 @@
+package IspitFinal;
+
+import java.util.Scanner;
+
+public class BasketballTournaments {
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+
+
+            }
+        }
+
